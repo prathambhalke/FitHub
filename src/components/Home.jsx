ad2a8a7e2c5860a78b9b7008d6404742999a298e
prathambhalke <prathameshbhalke125@gmail.com>
2023-09-18 23:53:27 +0530

    created three pages and added a Hero Banner Image 1> HomeBanner 2>SearchExercises 3>Exercises

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,13 @@
+import { Exercises, HomeBanner, SearchExercises } from "./";
+
 const Home = () => {
-  return <h1 className="text-3xl font-bold underline">FitHub!</h1>;
+  return (
+    <h1 className="text-3xl font-bold underline">
+      <HomeBanner />
+      <SearchExercises />
+      <Exercises />
+    </h1>
+  );
 };
 
 export default Home;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,6 @@
 export { default as Home } from "./Home";
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
+export { default as HomeBanner } from "./HomeBanner";
+export { default as Exercises } from "./Exercises";
+export { default as SearchExercises } from "./SearchExercises";
