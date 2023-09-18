a7deb8f4a6f7da1d7d5d4cee4ddcf0a723c61cbf
prathambhalke <prathameshbhalke125@gmail.com>
2023-09-18 23:42:41 +0530

    Updated Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,18 @@
+import { Link } from "react-router-dom";
 import Logo from "../assets/Logo.png";
 const Navbar = () => {
   return (
-    <nav className="bg-blue-500 p-4 font-Navbar text-white">
-      <div className="container mx-auto flex justify-start items-center">
+    <div className="flex justify-normal w-screen py-5 pl-5 ">
+      <Link to="/">
         <img src={Logo} alt="Logo" />
-        <div className="hidden md:flex space-x-4">
-          <p>Home</p>
-          <p>Exercises</p>
-        </div>
-        <div className="md:hidden">
-          {/* Mobile menu button */}
-          <button className="text-white">
-            <svg
-              className="h-6 w-6"
-              fill="none"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M4 6h16M4 12h16M4 18h16"></path>
-            </svg>
-          </button>
-        </div>
+      </Link>
+      <div className="flex justify-evenly items-end content w-full md:w-1/6 md:ml-28 ">
+        <Link to="/" className="border-b-2 border-b-red-700">
+          Home
+        </Link>
+        <a href="#exercises">Excercises</a>
       </div>
-    </nav>
+    </div>
   );
 };
 
