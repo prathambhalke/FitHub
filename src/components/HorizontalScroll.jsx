eb6fb1e285f133f19f39ec246defa243491fde70
prathambhalke <prathameshbhalke125@gmail.com>
2023-11-16 00:10:37 +0530

    #swiper js library added #autosliding carousel functionality added🚀

--- src/components/HorizontalScroll.jsx
+++ src/components/HorizontalScroll.jsx
@@ -1,37 +1,69 @@
-//commentented because will fetch and use
-// const filteredExercisesData = [...new Set(data.map((item) => item.bodyPart))];
+import { Swiper, SwiperSlide } from "swiper/react";
 
-// console.log(filteredExercisesData);
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
-import { FaDumbbell } from "react-icons/fa";
-import { GiGymBag } from "react-icons/gi";
-import { AiOutlineDoubleLeft, AiOutlineDoubleRight } from "react-icons/ai";
-import BodyPart from "./BodyPart";
+// import required modules
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 const horizontalScrollBar = ({ data, bodyPart, setBodyPart }) => {
   const filteredExercisesData = ["back", "shoulders", "legs", "arms"];
 
+  // //commentented because will fetch and use
+  // const filteredExercisesData = [...new Set(data.map((item) => item.bodyPart))];
+
+  // console.log(filteredExercisesData);
+
   return (
-    <div className="w-2/3 util-border m-24">
-      <div className="flex overflow-x-auto relative">
-        <div className="flex items-center">
-          <AiOutlineDoubleLeft className="relative" />
-        </div>
-        <div className="grid grid-cols-4 gap-8 p-4">
-          {" "}
-          {/* Increase the padding */}
-          {filteredExercisesData.map((item) => (
-            <div key={item} className="m-4">
-              {" "}
-              {/* Add margin to each card */}
-              <BodyPart bodyPartItem={item} />
-            </div>
-          ))}
-        </div>
-        <div className="flex items-center">
-          <AiOutlineDoubleRight />
-        </div>
-      </div>
+    // <div className="w-2/3 util-border m-24">
+    //   <div className="flex overflow-x-auto relative">
+    //     <div className="flex items-center">
+    //       <AiOutlineDoubleLeft className="relative" />
+    //     </div>
+    //     <div className="grid grid-cols-4 gap-8 p-4">
+    //       {" "}
+    //       {/* Increase the padding */}
+    //       {filteredExercisesData.map((item) => (
+    //         <div key={item} className="m-4">
+    //           {" "}
+    //           {/* Add margin to each card */}
+    //           <BodyPart bodyPartItem={item} />
+    //         </div>
+    //       ))}
+    //     </div>
+    //     <div className="flex items-center">
+    //       <AiOutlineDoubleRight />
+    //     </div>
+    //   </div>
+    // </div>
+    <div>
+      <Swiper
+        slidesPerView={4}
+        centeredSlides={true}
+        spaceBetween={30}
+        grabCursor={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper util-border m-24"
+      >
+        {filteredExercisesData.map((item) => {
+          console.log(item);
+          return (
+            <SwiperSlide className="util-border" key={item}>
+              {item}
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
     </div>
   );
 };
